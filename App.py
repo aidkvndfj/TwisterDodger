d981a07ec4b1fe5dd71b6525781198e3729a5879
aidkvndfj <eric8@bell.net>
2019-05-10 11:59:09 -0400

    Update gitignore, have the game loop untuill close, add time to a file

--- App.py
+++ App.py
@@ -2,5 +2,18 @@ import easygui as gui
 import Game
 import time
 
-gui.msgbox("Welcome to Twister Dodger", "Twister Dodger", "Play")
-print("\n\n\tYour Time Was: {0:0.2f}s\n\n".format(Game.Game()))
+scores = open("Scores.txt", "a+")
+
+while (True):
+    choice = gui.buttonbox("Welcome to Twister Dodger\nArrows Key To Move, Space To Jump", "Twister Dodger", ("Play", "Close"))
+    if (choice == "Play"):
+        time = Game.Game()
+        name = gui.enterbox("Your Time Was: {0:0.2f} seconds\nPlease Input Your Name:".format(time))
+        while (name == ""):
+            name = gui.enterbox("You Forgot Your Name\n\nYour Time Was: {0:0.2f} seconds\nPlease Input Your Name:".format(time))
+        scores.write("\n{0}\t{1:0.2f}s".format(name, time))
+
+
+    if (choice == "Close"):
+        break
+    choice = ""
--- Game.py
+++ Game.py
@@ -12,6 +12,7 @@ import random
 import sys
 import Eric_Dumb_Module as edm
 import time
+import easygui as gui
 
 # Class Imports
 sys.path.insert(0, 'Classes')
@@ -19,51 +20,53 @@ from DebrisClass import *
 from PlayerClass import *
 from TornadoClass import *
 
-# Constants
-global WIDTH, HEIGHT, FPS
-WIDTH = 1024
-HEIGHT = 576
-FPS = 30
-
-# Define Colors
-BACKGROUND = (160, 145, 250)
-RED        = (255, 0, 0)
-GRASS      = (40, 120, 20)
-GROUND     = (120, 70, 20)
-
-# Variables
-global groundHeight
-groundHeight = 120
-
-# Initialization
-pygame.init()
-pygame.font.init()
-
-# Screen Setup
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Twister Dodger")
-
-# Clock Setup
-clock = pygame.time.Clock()
-
-# Font Setup
-
-#~~~~~~~~~ Functions ~~~~~~~~~#
-def spawnDebris():
-    global WIDTH, HEIGHT, groundHeight
-    chance = random.randint(1, 12)
-    if (chance == 5):
-        debris = Debris(WIDTH, HEIGHT)
-        debrisSprites.add(debris)
-
-def drawGround():
-    global WIDTH, groundHeight
-    pygame.draw.rect(screen, GROUND, (0, HEIGHT - groundHeight, WIDTH, groundHeight))
-    pygame.draw.rect(screen, GRASS, (0, HEIGHT - groundHeight, WIDTH, 50))
-
 def Game():
+    # Constants
+    global WIDTH, HEIGHT, FPS
+    WIDTH = 1024
+    HEIGHT = 576
+    FPS = 30
+
+    # Define Colors
+    BACKGROUND = (160, 145, 250)
+    RED        = (255, 0, 0)
+    GRASS      = (40, 120, 20)
+    GROUND     = (120, 70, 20)
+
+    # Variables
+    global groundHeight
+    groundHeight = 120
+
+    # Initialization
+    pygame.init()
+    pygame.font.init()
+
+    # Screen Setup
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Twister Dodger")
+
+    # Clock Setup
+    clock = pygame.time.Clock()
+
+    # Font Setup
+    gameFont = pygame.font.Font(None, 25)
+    healthText = gameFont.render("Health:", True, (0, 0, 0))
+
+    #~~~~~~~~~ Functions ~~~~~~~~~#
+    def spawnDebris():
+        global WIDTH, HEIGHT, groundHeight
+        chance = random.randint(1, 12)
+        if (chance == 5):
+            debris = Debris(WIDTH, HEIGHT)
+            debrisSprites.add(debris)
+
+    def drawGround():
+        global WIDTH, groundHeight
+        pygame.draw.rect(screen, GROUND, (0, HEIGHT - groundHeight, WIDTH, groundHeight))
+        pygame.draw.rect(screen, GRASS, (0, HEIGHT - groundHeight, WIDTH, 50))
+
+
     start = time.time()
-    global groundHeight, FPS
     #~~~~~~~~ Sprite Init ~~~~~~~~#
     # Sprite Groups
     global debrisSprites
@@ -105,16 +108,21 @@ def Game():
         allSprites.update()
         debrisSprites.update()
 
+        # Update Text
+        end = time.time()
+        timeText = gameFont.render("Time: {0:0.1f}".format(end - start), True, (255, 255, 255))
+
         # Draw Frame
         screen.fill(BACKGROUND)
         debrisSprites.draw(screen)
         drawGround()
         allSprites.draw(screen)
-        pygame.draw.rect(screen, RED, (10, 10, player.health * 4, 30))
+        pygame.draw.rect(screen, RED, (10, 30, player.health * 4, 30))
+        screen.blit(healthText, (10, 10))
+        screen.blit(timeText, (WIDTH - 95, HEIGHT - 25))
 
         pygame.display.flip()
 
     pygame.font.quit()
     pygame.quit()
-    end = time.time()
     return end - start
